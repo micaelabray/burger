8f06993548f7b20f23c0c9f38f2897c408262d53
Micaela Bray <micaela.bray@gmail.com>
2019-08-01 20:07:01 -0500

    adds additional npm packages and sets up db files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Five Alarm', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Nash Vegas', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Moo Shroom', false);
--- server.js
+++ server.js
@@ -1,1 +1,3 @@
+var mysql = require('mysql');
 var express = require('express');
+var exphbs = require('express-handlebars');
